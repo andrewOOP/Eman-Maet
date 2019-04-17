b4f267b84d9ae147f2e4bf5c3917062b87c120de
LiaBarr <lia.barrack@eagles.oc.edu>
2019-04-17 13:59:52 -0500

    Remove User List Option for Employee

--- Eman-Maet/ClientApp/src/components/NavMenu.js
+++ Eman-Maet/ClientApp/src/components/NavMenu.js
@@ -6,7 +6,25 @@ import './NavMenu.css';
 
 export class NavMenu extends Component
 {
-  displayName = NavMenu.name
+    displayName = NavMenu.name
+
+    constructor(props) {
+        super(props);
+        this.state = { isAdmin: "" };
+
+        fetch('api/user/GetCurrentUser', {
+            method: 'GET',
+        })
+        .then(res => res.json())
+        .then(response => {
+            this.setState({ isAdmin: response.securityRole });
+            if (this.state.isAdmin === "Administrator") {
+                this.setState({ isAdmin: true });
+            }
+            else { this.setState({ isAdmin: false }); }
+        })
+            .catch(error => console.error('Error:', error));
+    }
 
     render()
     {
@@ -16,38 +34,39 @@ export class NavMenu extends Component
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-                <Nav>
-                    <LinkContainer to={'/myprofile'}>
+            <Nav>
+                <LinkContainer to={'/myprofile'}>
+                    <NavItem>
+                        <Glyphicon glyph='th-list' /> My Profile
+                    </NavItem>
+                </LinkContainer>
+			    <LinkContainer to={'/eventlist'}>
+				    <NavItem>
+					    <Glyphicon glyph='th-list' /> Event List
+				    </NavItem>
+                </LinkContainer>
+                {this.state.isAdmin &&
+                    <LinkContainer to={'/userlist'}>
                         <NavItem>
-                            <Glyphicon glyph='th-list' /> My Profile
-                </NavItem>
+                            <Glyphicon glyph='th-list' /> User List
+                        </NavItem>
                     </LinkContainer>
-			<LinkContainer to={'/eventlist'}>
-				<NavItem>
-					<Glyphicon glyph='th-list' /> Event List
-				</NavItem>
-			</LinkContainer>
-            <LinkContainer to={'/userlist'}>
-                <NavItem>
-                   <Glyphicon glyph='th-list' /> User List
-                </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/teamlist'}>
-                <NavItem>
-                   <Glyphicon glyph='th-list' /> Team List
-                </NavItem>
-            </LinkContainer>
-             <LinkContainer to={'/locationlist'}>
-                <NavItem>
-                   <Glyphicon glyph='th-list' /> Location List
-                </NavItem>
-                    </LinkContainer>
-             
-                </Nav>
+                }
+                <LinkContainer to={'/teamlist'}>
+                    <NavItem>
+                       <Glyphicon glyph='th-list' /> Team List
+                    </NavItem>
+                </LinkContainer>
+                <LinkContainer to={'/locationlist'}>
+                    <NavItem>
+                       <Glyphicon glyph='th-list' /> Location List
+                    </NavItem>
+                  </LinkContainer>
+            </Nav>
 
-                <Navbar.Brand>
-                    <Link to={'/'}>Log Out</Link>
-                </Navbar.Brand>
+            <Navbar.Brand>
+                <Link to={'/'}>Log Out</Link>
+            </Navbar.Brand>
         </Navbar.Collapse>
       </Navbar>
     );
